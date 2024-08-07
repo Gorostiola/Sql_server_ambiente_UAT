3c57590e6bf6891f9337100e32aee1cb1f81c860
Desarrollador1 <desarrollador911@gmail.com>
2024-08-07 14:05:35 -0600

    Ticket:INC1111
    Descripcion: Cambio desarrollador
    
    Fecha: 07-Agosto-2024
    
    Nombre: Desarrollador 1

--- Stored Procedure/Sp_autor.sql
+++ Stored Procedure/Sp_autor.sql
@@ -1,3 +1,4 @@
+--Desarrollador 1
 USE [bd1]
 GO
 /****** Object:  StoredProcedure [dbo].[Sp_autor]    Script Date: 06/08/2024 04:43:28 p. m. ******/
@@ -5,7 +6,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER proc [dbo].[Sp_autor](@Autor varchar(30))
+ALTER proc [dbo].[Sp_autor](@Autor varchar(60))
 as
 begin
 select * from libros
